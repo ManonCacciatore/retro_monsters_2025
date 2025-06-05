75174cda073858e99ef196f6870a89726c34ca93
“Lunoronti” <cacciatoremanon8@gmail.com>
2025-06-05 15:02:02 +0200

    Intégration templates et partials

--- app/config/params.php
+++ app/config/params.php
@@ -1,5 +1,8 @@
 <?php
 
+
+$content = "";
+
 // PARAMETRES DE CONNEXION
 define('DB_NAME', "retro_monsters_2025");
 define('DB_HOST', "localhost:3307");
--- app/views/templates/index.php
+++ app/views/templates/index.php
@@ -2,15 +2,16 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include_once "../app/views/templates/partials/_head.php"; ?>
 </head>
 
-<body>
-
-    <p>coucou</p>
-
+<body class="bg-gray-800 text-white font-sans">
+    <!-- Header -->
+    <?php include_once "../app/views/templates/partials/_header.php"; ?>
+    <!-- Main Content -->
+    <?php include_once "../app/views/templates/partials/_main.php"; ?>
+    <!-- Footer -->
+    <?php include_once "../app/views/templates/partials/_footer.php"; ?>
 </body>
 
 </html>
